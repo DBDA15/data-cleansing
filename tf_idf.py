0da21ea72c373bbf7ac22ed330c97f71c13bf885
Willi <willi@jups42.de>
2015-04-24 18:34:01 +0200

    tfidf algorithm with 2 nested loops, however illegal to iterate over 2 rdds at the same time

--- tf_idf.py
+++ tf_idf.py
@@ -1,18 +1,53 @@
 import math
+from collections import Counter
 
 
-def tfidf(term, doc, corpus):
-    return tf(term, doc) * idf(term, corpus)
+def get_term_dict(corpus):
+    return corpus.distinct()
 
 
-def idf(term, corpus):
-    corpus = corpus.map(lambda line:  line.split('"')[1] if line != u'# started 2014-08-21T14:51:59Z' else '')
-    doc_count = corpus.count()
-    docs_with_term = corpus.map(lambda d: int(term in d)).sum()
-    if docs_with_term:
-        return math.log(doc_count / float(docs_with_term))
+def get_all_docs(corpus):
+    return corpus.map(lambda line:  line.split('"')[1]
+                      if line != u'# started 2014-08-21T14:51:59Z'
+                      else '')
+
+
+def calc_tfidf(rdd):
+    # corpus = sc.textFile(path)
+    corpus = rdd
+    docs = get_all_docs(corpus)
+    docs.cache()
+    doc_count = docs.count()
+    term_dict = get_term_dict(corpus)
+    term_dict.cache()
+    dwt = n_docs_with_term(docs, term_dict)
+    return docs.foreach(lambda d: iterate_docs(d, doc_count, term_dict))
+
+
+def n_docs_with_term(docs, term_dict):
+    term_dict.foreach(lambda term: docs.map(
+                        lambda d: int(term in d)).sum())
+
+
+def iterate_docs(doc, doc_count, term_dict):
+    words_in_doc = doc.split().count()
+    vector = []
+    return term_dict.foreach(lambda term:
+                             iterate_terms(doc, term, doc_count, term_dict))
+
+
+def iterate_terms(doc, term, term_dict, doc_count):
+    tf = calc_tf(term, doc)
+    idf = calc_idf(docs, doc_count, term_dict)
+    return tf * idf
+
+
+def calc_idf(docs, doc_count, term_dict):
+    n = n_docs_with_term(docs, term_dict)
+    if n:
+        return math.log(doc_count / float(n))
     return 0
 
 
-def tf(word, doc):
+def calc_tf(word, doc):
     return doc.split().count(word) / float(len(doc.split()))
