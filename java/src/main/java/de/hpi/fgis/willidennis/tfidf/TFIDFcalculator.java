f6500484bf4f3e7130e002add706d79da11f0dc5
dennis.wolf <dennis@dennis-VirtualBox.(none)>
2015-04-26 22:33:48 +0200

    Java+Maven Hello World w/ Spark dependency 	new file:   .gitignore 	new file:   pom.xml 	new file:   src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java

---
+++ java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
@@ -0,0 +1,13 @@
+package de.hpi.fgis.willidennis.tfidf;
+
+public class TFIDFcalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
