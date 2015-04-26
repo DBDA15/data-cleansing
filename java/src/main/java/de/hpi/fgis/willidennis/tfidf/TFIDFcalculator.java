27436a2e45e24f8abf4cfa064af0c767f1ceedbb
dennis.wolf <dennis@dennis-VirtualBox.(none)>
2015-04-27 00:10:19 +0200

    split lines to make abstract text available in LineItem object use key-value pair rdd <String, LineItem>, key(String) is the first <> of each line

--- java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
+++ java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
@@ -1,14 +1,20 @@
 package de.hpi.fgis.willidennis.tfidf;
 
 import java.io.Serializable;
+import java.util.Arrays;
 //import java.util.Comparator;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 //import org.apache.spark.api.java.function.Function;
 //import org.apache.spark.api.java.function.Function2;
 //import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
 
 
 public class TFIDFcalculator {
@@ -24,10 +30,31 @@ public class TFIDFcalculator {
 	    config.set("spark.hadoop.validateOutputSpecs", "false");
 	    try(JavaSparkContext context = new JavaSparkContext(config)) {
 	    	// load lineitems
-	        JavaRDD<String> lines = context.textFile(filepath);
-	        System.out.println(lines.count());
+	        JavaPairRDD<String, LineItem> lineItems = context.
+	        		textFile(filepath)
+	        		.mapToPair(
+	        				new PairFunction<String, String, LineItem>() {
+	        					public Tuple2<String, LineItem> call(String line) {
+	        						LineItem li = new LineItem(line);
+	        						return new Tuple2<String, LineItem>(li.stringid, li);
+	        					}
+	        				});
 	    	}
 	 }
 	
+	
+	static class LineItem implements Serializable {
+	    public String stringid;
+	    public String text;
+
+	    public LineItem(String line) {
+	      String[] values = line.split("<");
+	      stringid = values[1];
+	      text = values[2].split("\"")[1];
+	    }
+	    
+	    public String toString() { return stringid + ": " + text; }
+	    
+	  }
 
 }
