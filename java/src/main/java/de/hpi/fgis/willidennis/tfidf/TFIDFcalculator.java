cbe0cb3d582a86b82b950318f972b9cf7d563223
dennis.wolf <dennis@dennis-VirtualBox.(none)>
2015-04-26 23:40:00 +0200

    reads file and outputs number of lines

--- java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
+++ java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
@@ -1,13 +1,33 @@
 package de.hpi.fgis.willidennis.tfidf;
 
+import java.io.Serializable;
+//import java.util.Comparator;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+//import org.apache.spark.api.java.function.Function;
+//import org.apache.spark.api.java.function.Function2;
+//import org.apache.spark.api.java.function.PairFunction;
+
+
 public class TFIDFcalculator {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
+		String filepath = args[0];
+		
+		// initialize spark environment
+	    SparkConf config = new SparkConf().setAppName(TFIDFcalculator.class.getName());
+	    config.set("spark.hadoop.validateOutputSpecs", "false");
+	    try(JavaSparkContext context = new JavaSparkContext(config)) {
+	    	// load lineitems
+	        JavaRDD<String> lines = context.textFile(filepath);
+	        System.out.println(lines.count());
+	    	}
+	 }
+	
 
 }
