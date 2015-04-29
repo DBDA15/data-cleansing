eaf924aefe35cd29399e7789af73fc95ce44692a
Willi <willi@jups42.de>
2015-04-29 14:42:03 +0200

    java version 7 in maven

--- java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
+++ java/src/main/java/de/hpi/fgis/willidennis/tfidf/TFIDFcalculator.java
@@ -27,19 +27,19 @@ import scala.Tuple2;
 
 
 public class TFIDFcalculator {
-	
+
 	// from https://github.com/harryaskham/Twitter-L-LDA/blob/master/util/Stopwords.java
 	public static String[] stopwords = {"a", "as", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", "zero"};
 	public static Set<String> stopWordSet = new HashSet<String>(Arrays.asList(stopwords));
-	
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String filepath = args[0];
-		
-		
+
+
 		// initialize spark environment
 	    SparkConf config = new SparkConf().setAppName(TFIDFcalculator.class.getName());
 	    config.set("spark.hadoop.validateOutputSpecs", "false");
@@ -54,9 +54,9 @@ public class TFIDFcalculator {
 	        						return new Tuple2<String, LineItem>(li.stringid, li);
 	        					}
 	        				});
-	        
-	        
-	        
+
+
+
 	        // in how many documents does each term occur?
 	        // as in https://spark.apache.org/docs/0.9.1/java-programming-guide.html
 	        JavaPairRDD<String, Integer> wordOccurrences = lineItems.values().flatMap(
@@ -81,9 +81,9 @@ public class TFIDFcalculator {
 
 	        List<Tuple2<String, Integer>> wordOccsList = wordOccurrences.collect();
 	        final Broadcast<List<Tuple2<String, Integer>>> bcvar = context.broadcast(wordOccsList);
-	        
+
 	        final Broadcast<Long> bctotalnumberofdocs = context.broadcast(lineItems.count());
-	        
+
 	        lineItems = lineItems.mapValues(
     				new Function<LineItem, LineItem>() {
     					public LineItem call(LineItem li) {
@@ -91,15 +91,13 @@ public class TFIDFcalculator {
     						return li;
     					}
     				});
-	        
+
 	        System.out.println(lineItems.take(10));
-	        
+
 	    }
-	    
-	    
 	 }
-	
-	
+
+
 	static class LineItem implements Serializable {
 	    public String stringid;
 	    public HashMap<String, Integer> wordmap;
@@ -110,10 +108,10 @@ public class TFIDFcalculator {
 	      String[] values = line.split("<");
 	      stringid = values[1];
 	      String text = values[2].split("\"")[1];
-	      
+
 	      String[] words = text.split(" ");
 	      wordcount = words.length;
-	      
+
 	      wordmap = new HashMap<String, Integer>();
 	      for(String word: words) {
 	    	  if(!isStopword(word)) {
@@ -127,17 +125,17 @@ public class TFIDFcalculator {
 
 		public void calcTFIDFs(Broadcast<List<Tuple2<String, Integer>>> bcvar, Broadcast<Long> totalnumberofdocs) {
 			tfidfs = new HashMap<String, Double>();
-			
+
 			// which term is most often in this doc and how often is that?
 			int maxCountInThisDoc = -1;
 			for(String term: wordmap.keySet()) {
 				int thisCount = wordmap.get(term);
 				if(thisCount>maxCountInThisDoc)  maxCountInThisDoc = thisCount;
 			}
-			
+
 			for(String term: wordmap.keySet()) {
 				int countThisDoc = wordmap.get(term);
-				
+
 				int countInDocs = -1;
 				Iterator myiter = bcvar.getValue().iterator();
 				boolean found = false;
@@ -148,20 +146,20 @@ public class TFIDFcalculator {
 						found = true;
 					}
 				} while (!found && myiter.hasNext());
-				
+
 				double tf = countThisDoc / maxCountInThisDoc;
-				double idf = Math.log(totalnumberofdocs.getValue() / countInDocs);
-				double tfidf = tf*idf;
-				
+				double idf = totalnumberofdocs.getValue() / countInDocs;
+				double tfidf = tf/idf;
+
 				tfidfs.put(term, tfidf);
 			}
-			
+
 		}
 
 		public String toString() { return stringid + ": " + tfidfs; }
-	    
+
 	  }
-	
+
 	/**
 	 * src: https://github.com/harryaskham/Twitter-L-LDA/blob/master/util/Stopwords.java
 	 * @param word
