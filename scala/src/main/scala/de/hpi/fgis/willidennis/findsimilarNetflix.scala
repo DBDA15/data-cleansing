2e6334e77869c0f510f401b1ae918b2c069a348b
Dennis <dennis.wolf0815@gmail.com>
2015-05-26 16:57:15 +0200

    added main method

--- scala/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
+++ scala/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
@@ -12,8 +12,6 @@ import Array._
 
 object Main extends App {
 
-	val numberOfFiles = 4
-	val numberOfMoviesForSig = 2
 	val simThreshold = 0.5
 
 	def determineSignature ( n:Int, statistics:Array[Int], ratings: Iterable[(Int, Int, Int)] ) : String = {
@@ -46,7 +44,7 @@ object Main extends App {
 			/* compare with all elements that follow */
 			for(n<-(i+1) to (candidates.length-1)) {
 				var compareTo = candidates(n)
-				
+
 				/* calculate similarity and add to result */
 				val simvalue = calculateSimilarity(element, compareTo)
 				if(simvalue > simThreshold)	{
@@ -62,61 +60,69 @@ object Main extends App {
 		return (splitted(0).toInt, movid, splitted(1).toInt) // (userid, movid, rating)
 	}
 
-	var TRAINING_PATH = "netflixdata/training_set/"
+	
+	override def main(args: Array[String]) = {
+		val numberOfFiles = 4
+		val numberOfMoviesForSig = 2
 
-	var conf = new SparkConf()
-	conf.setAppName(Main.getClass.getName)
-	conf.set("spark.executor.memory", "4g")
-	val sc = new SparkContext(conf)
+		var TRAINING_PATH = "netflixdata/training_set/"
+		//if(!args.isEmpty) TRAINING_PATH = args(1)
+		println(args)
 
-	// build empty RDD, not that empty though
-	var parsed = sc.textFile(TRAINING_PATH+"mv_0000001.txt").filter(!_.contains(":")).map(line => parseLine(line, 1))
+		var conf = new SparkConf()
+		conf.setAppName(Main.getClass.getName)
+		conf.set("spark.executor.memory", "4g")
+		val sc = new SparkContext(conf)
 
-	for(i <- 2 to numberOfFiles) {
-		val thisDataset = sc.textFile(TRAINING_PATH+"mv_" + "%07d".format(i) + ".txt").filter(!_.contains(":")).map(line => parseLine(line, i))
-		parsed = parsed ++ thisDataset
+		// build empty RDD, not that empty though
+		var parsed = sc.textFile(TRAINING_PATH+"mv_0000001.txt").filter(!_.contains(":")).map(line => parseLine(line, 1))
+
+		for(i <- 2 to numberOfFiles) {
+			val thisDataset = sc.textFile(TRAINING_PATH+"mv_" + "%07d".format(i) + ".txt").filter(!_.contains(":")).map(line => parseLine(line, i))
+			parsed = parsed ++ thisDataset
+		}
+
+		/* statistics */
+		/* TODO: these statistics could also be generated while reading the files! */
+		val statistics = parsed.groupBy(_._2).map(x => (x._1, x._2.size)).sortBy(_._2)
+
+		/* group ratings by user */
+		val users = parsed.groupBy(_._1) /* users: org.apache.spark.rdd.RDD[(Int, Iterable[(Int, Int, Int)])] */
+		/* TODO: users has userID as key and then repeted in tuple. unnecessary! */
+
+
+		/* make signature
+		*
+		* Broadcast
+		* N: how many movie ids are used for the sig
+		*/
+
+		val bcCount = sc.broadcast(statistics.map(x => x._1).collect) /* we only keep the movid, not the number of ratings */
+
+
+		/* 	yields RDD[(signature, user)].
+		*	user represented by iterable of all his ratings.
+		*	formally RDD[(String, Array[(Int, Iterable[(Int, Int, Int)])])]
+		*/
+		val signed = users.map( x => (determineSignature(numberOfMoviesForSig, bcCount.value, x._2), Array(x)) ).filter(_._1 != null)
+
+
+		/* reduce: create Array[all users with same signature]
+			(key is dropped because we dont need it anymore)
+			yields RDD[Array[Array[(Int, Iterable[(Int, Int, Int)])]]
+			interpreted as follows:
+				inner Array is one user represented by all his votings
+				outer array is a bucket of all users with the same signature. (we dropped the sig string bfore)
+				RDD is the list of buckets by signature
+			reduced.count : how many movie(-combinations) have produced a bucket of candidates?
+			reduced.map(x => (x.size)).collect: How big are the buckets?
+				Important to anticipate runtime of compareCandidates!
+		*/
+
+		val reduced = signed.reduceByKey((a,b) => concat(a,b)).values /* RDD[Array[(Int, Iterable[(Int, Int, Int)])] */
+
+		val similarities = reduced.flatMap(compareCandidates)
+		println(similarities.take(50))
+		similarities.saveAsTextFile("result")
 	}
-
-	/* statistics */
-	/* TODO: these statistics could also be generated while reading the files! */
-	val statistics = parsed.groupBy(_._2).map(x => (x._1, x._2.size)).sortBy(_._2)
-
-	/* group ratings by user */
-	val users = parsed.groupBy(_._1) /* users: org.apache.spark.rdd.RDD[(Int, Iterable[(Int, Int, Int)])] */
-	/* TODO: users has userID as key and then repeted in tuple. unnecessary! */
-
-
-	/* make signature
-	*
-	* Broadcast
-	* N: how many movie ids are used for the sig
-	*/
-
-	val bcCount = sc.broadcast(statistics.map(x => x._1).collect) /* we only keep the movid, not the number of ratings */
-
-
-	/* 	yields RDD[(signature, user)].
-	*	user represented by iterable of all his ratings.
-	*	formally RDD[(String, Array[(Int, Iterable[(Int, Int, Int)])])]
-	*/
-	val signed = users.map( x => (determineSignature(numberOfMoviesForSig, bcCount.value, x._2), Array(x)) ).filter(_._1 != null)
-
-
-	/* reduce: create Array[all users with same signature]
-		(key is dropped because we dont need it anymore)
-		yields RDD[Array[Array[(Int, Iterable[(Int, Int, Int)])]]
-		interpreted as follows:
-			inner Array is one user represented by all his votings
-			outer array is a bucket of all users with the same signature. (we dropped the sig string bfore)
-			RDD is the list of buckets by signature
-		reduced.count : how many movie(-combinations) have produced a bucket of candidates?
-		reduced.map(x => (x.size)).collect: How big are the buckets?
-			Important to anticipate runtime of compareCandidates!
-	*/
-
-	val reduced = signed.reduceByKey((a,b) => concat(a,b)).values /* RDD[Array[(Int, Iterable[(Int, Int, Int)])] */
-
-	val similarities = reduced.flatMap(compareCandidates)
-	println(similarities.take(50))
-	similarities.saveAsTextFile("result")
 }
