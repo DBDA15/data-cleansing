624342ca6fd3717e743a8d441d035dbe382c8a2e
Dennis <dennis.wolf0815@gmail.com>
2015-06-01 01:09:59 +0200

    added time-statistics

--- scala/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
+++ scala/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
@@ -75,6 +75,8 @@ object Main extends App {
 
 
 	override def main(args: Array[String]) = {
+		val timeAtBeginning = System.currentTimeMillis
+
 		var firstNLineOfFile = -1
 		var numberOfFiles = 4
 		var numberOfMoviesForSig = 2
@@ -151,7 +153,7 @@ object Main extends App {
 		//reduced.map(x => (x.size)).saveAsTextFile(RESULTS_PATH)
 		//calcStatistics.saveAsTextFile(RESULTS_PATH)
 				
-		println(s"\n\n ####### Ratings: ${parsed.count()} in ${numberOfFiles} files ###### \n\n")
+		println(s"\n\n ####### Ratings: ${parsed.count()} in ${numberOfFiles} files, ${(System.currentTimeMillis-timeAtBeginning)/1000}s ###### \n\n")
 		//println(s"\n\n ####### Users-Signatures: ${signed.count()} ###### \n\n")
 		println(s"\n\n ####### Statistics: ${statistics(2)} | ${statistics(1)} | ${statistics(0)} ###### \n\n")
 	}
