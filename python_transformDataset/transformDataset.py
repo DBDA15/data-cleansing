1ce06212076f119afbfdd670313d72d62acf743c
Willi <willi@jups42.de>
2015-06-29 15:27:54 +0200

    fix path + formatting for python 2.7

--- python_transformDataset/transformDataset.py
+++ python_transformDataset/transformDataset.py
@@ -3,13 +3,13 @@ MAXFILES = 17770 # 17770 is max in dataset
 PRINTREADEVERY = 100
 PRINTWRITEEVERY = 100000
 
-dir = 'D:/Eigene Dateien/HPI/DBDA/prize_dataset/download/training_set/training_set/'
+directory = '../netflixdata/training_set/'
 
 print('reading...')
 currentFileIndex = 1
 while currentFileIndex <= MAXFILES:
 	filename = 'mv_' + '{0:07d}'.format(currentFileIndex) + '.txt'
-	mFile = open(dir + filename ,'r')
+	mFile = open(directory + filename ,'r')
 	movid = 'TOBEFOUND'
 	for line in mFile:
 		if(':' in line): movid = int(line.split(':')[0])
@@ -22,8 +22,8 @@ while currentFileIndex <= MAXFILES:
 			else:
 				targetDict[userid].append(ratingTuple)
 	mFile.close()
-	if(currentFileIndex % PRINTREADEVERY == 0): 
-		percentageString = '{0:03f}'.format(currentFileIndex/MAXFILES*100)
+	if(currentFileIndex % PRINTREADEVERY == 0):
+		percentageString = '{0:03f}'.format(currentFileIndex/float(MAXFILES)*100)
 		print(str(currentFileIndex) + ' done (' + percentageString + ' %)')
 	currentFileIndex += 1
 
@@ -35,9 +35,9 @@ numberOfUsers = len(targetDict)
 for key in targetDict:
 	fileout.write(str(targetDict[key]) + '\n')
 	writeCount += 1
-	
-	if(writeCount % PRINTWRITEEVERY == 0): 
-		percentageString = '{0:03f}'.format(writeCount/numberOfUsers*100)
+
+	if(writeCount % PRINTWRITEEVERY == 0):
+		percentageString = '{0:03f}'.format(writeCount/float(numberOfUsers)*100)
 		print(str(writeCount) + ' done (' + percentageString + ' %)')
 fileout.close()
 
