050170af7abb34afef31ace1908eb28e9104f7b2
Willi <willi@jups42.de>
2015-06-29 16:58:07 +0200

    memory-efficient db transform

--- python_transformDataset/transformDataset.py
+++ python_transformDataset/transformDataset.py
@@ -1,44 +1,48 @@
+import csv
 targetDict = {}
-MAXFILES = 17770 # 17770 is max in dataset
+MAXFILES = 17770  # 17770 is max in dataset
 PRINTREADEVERY = 100
-PRINTWRITEEVERY = 100000
+NUMBER_OF_FILES = 1000
 
 directory = '../netflixdata/training_set/'
 
 print('reading...')
-currentFileIndex = 1
-while currentFileIndex <= MAXFILES:
+
+
+def write_ratings(targetDict):
+	print('writing...')
+	for key in targetDict:
+		filename = key % NUMBER_OF_FILES
+		with open("%sby_user/%i.csv" % (directory, filename), "a") as csvfile:
+			writer = csv.writer(csvfile)
+			ratings = targetDict[key]
+			for r in ratings:
+				writer.writerow(r)
+	print("written")
+
+
+for currentFileIndex in range(1, MAXFILES):
 	filename = 'mv_' + '{0:07d}'.format(currentFileIndex) + '.txt'
-	mFile = open(directory + filename ,'r')
-	movid = 'TOBEFOUND'
-	for line in mFile:
-		if(':' in line): movid = int(line.split(':')[0])
-		else:
-			userid = int(line.split(',')[0])
-			rating = int(line.split(',')[1])
-			ratingTuple = (userid,movid,rating)
-			if not userid in targetDict:
-				targetDict[userid] = [ratingTuple]
+	with open(directory + filename,'r') as mFile:
+		movid = 'TOBEFOUND'
+		for line in mFile:
+			if(':' in line): movid = int(line.split(':')[0])
 			else:
-				targetDict[userid].append(ratingTuple)
-	mFile.close()
+				userid = int(line.split(',')[0])
+				rating = int(line.split(',')[1])
+				ratingTuple = (userid,movid,rating)
+				if not userid in targetDict:
+					targetDict[userid] = [ratingTuple]
+				else:
+					targetDict[userid].append(ratingTuple)
+
 	if(currentFileIndex % PRINTREADEVERY == 0):
 		percentageString = '{0:03f}'.format(currentFileIndex/float(MAXFILES)*100)
 		print(str(currentFileIndex) + ' done (' + percentageString + ' %)')
-	currentFileIndex += 1
+		write_ratings(targetDict)
+		targetDict.clear()
 
-print('writing...')
-fileout = open('_output.txt','w')
-writeCount = 0
-numberOfUsers = len(targetDict)
 
-for key in targetDict:
-	fileout.write(str(targetDict[key]) + '\n')
-	writeCount += 1
 
-	if(writeCount % PRINTWRITEEVERY == 0):
-		percentageString = '{0:03f}'.format(writeCount/float(numberOfUsers)*100)
-		print(str(writeCount) + ' done (' + percentageString + ' %)')
-fileout.close()
 
 print('finished')
