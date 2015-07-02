e79371d89a330c14976f38bd92b95720ce2f373d
Dennis <dennis.wolf0815@gmail.com>
2015-07-02 23:53:44 +0200

    removed unused code

--- flink/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
+++ flink/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
@@ -27,52 +27,9 @@ object Main extends App {
 		return aList.combinations(n)
 	}
 
-	def calculateSimilarity(user1: Iterable[Rating], user2: Iterable[Rating]) : Double = {
-		val u1set = user1.map(x => (x.movie, x.stars)).toSet
-		val u2set = user2.map(x => (x.movie, x.stars)).toSet
-
-		return u1set.intersect(u2set).size.toDouble / u1set.union(u2set).size.toDouble
-	}
-
-
 	def parseLine(line: String):Rating = {
 		val splitted = line.split(",")
 		return Rating(splitted(0).toInt, splitted(1).toInt, splitted(2).toInt)
-	}
-
-	def compareCandidates(config:Config, candidatesArray: Array[ Array[Rating] ]): Array[(String, Long)] = {
-		var numberOfSims = 0.toLong
-		var comparisonsRaw = 0.toLong
-		var comparisonsEffective = 0.toLong
-		
-		for(i<-0 to (candidatesArray.length-2)) {
-			var user1 = candidatesArray(i)
-
-			/* compare with all elements that follow */
-			for(n<-(i+1) to (candidatesArray.length-1)) {
-				var user2 = candidatesArray(n)
-
-				/* calculate similarity and add to result if sizes are close enough (depends on SIMTHRESHOLD) */
-				var sizesInRange = false
-				if(user1.size<user2.size) {
-					sizesInRange = user2.size*config.SIM_THRESHOLD <= user1.size
-				} else {
-					sizesInRange = user1.size*config.SIM_THRESHOLD <= user2.size
-				}
-
-				comparisonsRaw += 1
-
-				if(sizesInRange) {
-					val simvalue = calculateSimilarity(user1, user2)
-					comparisonsEffective += 1
-					if(simvalue >= config.SIM_THRESHOLD) {
-						numberOfSims += 1
-					}
-				}
-			}
-		}
-		return Array(	("similarities", numberOfSims), ("unpruned comparisons", comparisonsRaw),
-			("comps after length filter", comparisonsEffective))
 	}
 
 	def groupAllUsersRatings(SIMTHRESHOLD:Double, SIGNATURE_SIZE:Int, in: Iterator[Rating], out: Collector[(String, Int)])  {
@@ -143,14 +100,6 @@ object Main extends App {
 		}
 	}
 
-	def outputStats(config:Config, similar:DataSet[Array[(String, Long)]]) = {
-		val aggregatedStats = similar.reduce {
-			(x: Array[(String, Long)], y: Array[(String, Long)]) =>
-				Array( (x(0)._1, x(0)._2 + y(0)._2), (x(1)._1, x(1)._2 + y(1)._2), (x(2)._1, x(2)._2 + y(2)._2))
-		}
-		val printableAggregatedStats = aggregatedStats.map(_.toList)
-		printableAggregatedStats.writeAsText(config.STAT_FILE, writeMode=FileSystem.WriteMode.OVERWRITE)
-	}
 
 	def run(config: Config) {
 		val timeAtBeginning = System.currentTimeMillis
@@ -164,15 +113,15 @@ object Main extends App {
 		//signed.writeAsCsv("file:///tmp/flink-user", writeMode=FileSystem.WriteMode.OVERWRITE)
 
 		val SIGNATURE = 0
-		val similar = signed.groupBy(SIGNATURE).reduceGroup {
+		val bucketSizes = signed.groupBy(SIGNATURE).reduceGroup {
 			(in:  Iterator[ (String, Int) ], out: Collector[ (String, Int) ])  =>
 				val all = in.toList
 				val signature = all(0)._1
 				out.collect((signature, all.length))
 		}
-		similar.writeAsText(config.OUTPUT_FILE, writeMode=FileSystem.WriteMode.OVERWRITE)
+
+		bucketSizes.writeAsText(config.OUTPUT_FILE, writeMode=FileSystem.WriteMode.OVERWRITE)
 		//println(env.getExecutionPlan())
-		//outputStats(config, similar)
 		env.execute(config.EXECUTION_NAME)
 		println(s"time: ${System.currentTimeMillis - timeAtBeginning}")
 	}
