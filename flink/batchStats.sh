70299f3a28fea66a5ae9a7d4041b2ae59b7cc972
Willi <willi@jups42.de>
2015-07-08 22:32:25 +0200

    flink: STATS run script

--- flink/batchStats.sh
+++ flink/batchStats.sh
@@ -1,34 +1,51 @@
-pathToBinFlink="/opt/flink/flink-0.9.0/bin/flink"
-pathToJars="target"
-MY_TRAINING_PATH="hdfs://tenemhead2/data/data-cleansing/netflixdata/training_set/cat"
+FLINK="/opt/flink/flink-0.9.0/bin/flink"
+JAR="target/findSimilarNetflix-1.0.jar"
+INPUT="hdfs://tenemhead2/data/data-cleansing/netflixdata/training_set/cat"
 LOG_DIR="out/mylogs"
 OUTPUT_DIR="hdfs://tenemhead2/data/data-cleansing/out"
 BUILD="1.0"
 
 sigSize=1
 cores=20
+files=200
 
-$pathToBinFlink run --class de.hpi.fgis.willidennis.Main \
-		$pathToJars/findSimilarNetflix-$BUILD.jar \
-		--TRAINING_PATH $MY_TRAINING_PATH$files/ \
-		--SIGNATURE_SIZE $sigSize --FILES 1 \
-		--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f$files$cores" \
-		--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores" \
-		--CORES $cores \
-		--USE_LENGTH_CLASSES_IN_SIG \
-		> "$LOG_DIR/log-findSimilars-s$sigSize$files$cores"
+for flag in "" --USE_LENGTH_CLASSES_IN_SIG
+	echo "collect similars s$sigSize f$files c$cores $flag start:" $(date +"%T")
+	$FLINK run --class de.hpi.fgis.willidennis.Main \
+	$JAR \
+	--TRAINING_PATH $INPUT$files/ \
+	--SIGNATURE_SIZE $sigSize --FILES 1 \
+	--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f${files}c${cores}$flag" \
+	--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores$flag" \
+	--CORES $cores \
+	$flag \
+	> "$LOG_DIR/log-findSimilars-s${sigSize}f${files}c$cores$flag"
 
+files=100
 for cores in 20 10 4 2 1
 do
-	echo "collect similars s$sigSize f$files c$cores start:" $(date +"%T")
-	$pathToBinFlink run --class de.hpi.fgis.willidennis.Main \
-	$pathToJars/findSimilarNetflix-377eaf-findSimilars.jar \
-	--TRAINING_PATH $MY_TRAINING_PATH$files/ \
+	echo "collect similars s$sigSize f$files c$cores $flag start:" $(date +"%T")
+	$FLINK run --class de.hpi.fgis.willidennis.Main \
+	$JAR \
+	--TRAINING_PATH $INPUT$files/ \
 	--SIGNATURE_SIZE $sigSize --FILES 1 \
-	--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f$files$cores" \
-	--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores" \
+	--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f${files}c${cores}$flag" \
+	--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores$flag" \
 	--CORES $cores \
-	--USE_LENGTH_CLASSES_IN_SIG \
-	> "$LOG_DIR/log-findSimilars-s$sigSize$files$cores"
+	> "$LOG_DIR/log-findSimilars-s${sigSize}f${files}c$cores$flag"
 done
+
+files=500
+for flag in "" --USE_LENGTH_CLASSES_IN_SIG
+	echo "collect similars s$sigSize f$files c$cores $flag start:" $(date +"%T")
+	$FLINK run --class de.hpi.fgis.willidennis.Main \
+	$JAR \
+	--TRAINING_PATH $INPUT$files/ \
+	--SIGNATURE_SIZE $sigSize --FILES 1 \
+	--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f${files}c${cores}$flag" \
+	--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores$flag" \
+	--CORES $cores \
+	$flag \
+	> "$LOG_DIR/log-findSimilars-s${sigSize}f${files}c$cores$flag"
+
 echo "finish:" $(date +"%T")
