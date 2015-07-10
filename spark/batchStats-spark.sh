f143ce7b2216fe6b0cd28591419e9d6ca6657e9a
Willi <willi@jups42.de>
2015-07-10 22:15:24 +0200

    spark: script to run locally

---
+++ spark/batchStats-spark.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# SPARK="/opt/spark/spark-1.3.0/bin/spark-submit"
+SPARK=spark-submit
+JAR="target/findSimilarNetflix-1.0.jar"
+#INPUT="hdfs://tenemhead2/data/data-cleansing/netflixdata/training_set/cat"
+INPUT="../netflixdata/cat"
+# LOG_DIR="out/mylogs/spark"
+LOG_DIR="."
+#OUTPUT_DIR="hdfs://tenemhead2/data/data-cleansing/spark/out"
+OUTPUT_DIR="/tmp/spark-output"
+BUILD="1.0"
+# MASTER="spark://172.16.21.111:7077"
+MASTER="localhost:4041"
+
+sigSize=1
+flag=""
+files=10
+for cores in 20 10 4 2 1
+do
+	echo "collect similars s$sigSize f$files c$cores $flag start:" $(date +"%T")
+	$SPARK --class de.hpi.fgis.willidennis.Main \
+	--master $MASTER \
+	--conf spark.cores.max=$cores \
+	$JAR \
+	--TRAINING_PATH $INPUT$files/ \
+	--SIGNATURE_SIZE $sigSize --FILES 1 \
+	--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f${files}c${cores}$flag" \
+	--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores$flag" \
+	--CORES $cores \
+	> "$LOG_DIR/log-findSimilars-s${sigSize}f${files}c$cores$flag"
+done
+
+files=500
+cores=20
+
+echo "collect similars s$sigSize f$files c$cores $flag start:" $(date +"%T")
+$SPARK --class de.hpi.fgis.willidennis.Main \
+--master $MASTER \
+--conf spark.cores.max=$cores \
+$JAR \
+--TRAINING_PATH $INPUT$files/ \
+--SIGNATURE_SIZE $sigSize --FILES 1 \
+--OUTPUT_FILE "$OUTPUT_DIR/similars-s${sigSize}f${files}c${cores}$flag" \
+--EXECUTION_NAME "data-cleansing-findSimilars-s${sigSize}f${files}c$cores$flag" \
+--CORES $cores \
+> "$LOG_DIR/log-findSimilars-s${sigSize}f${files}c$cores$flag"
+
+echo "finish:" $(date +"%T")
