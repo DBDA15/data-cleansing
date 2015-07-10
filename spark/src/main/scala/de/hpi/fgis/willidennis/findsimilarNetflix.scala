636f7325c182ca828341bf6856c8714a738f2b24
Willi <willi@jups42.de>
2015-07-10 21:48:22 +0200

    spark: set execution name

--- spark/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
+++ spark/src/main/scala/de/hpi/fgis/willidennis/findsimilarNetflix.scala
@@ -261,7 +261,7 @@ object Main extends App {
 
 	def configureSpark(conf: Config): SparkContext = {
 		var sparkConf = new SparkConf()
-		sparkConf.setAppName(Main.getClass.getName)
+		sparkConf.setAppName(conf.EXECUTION_NAME)
 		sparkConf.set("spark.executor.memory", conf.MEMORY)
 		sparkConf.set("spark.cores.max", conf.CORES.toString)
 		sparkConf.setMaster(conf.MASTER)
