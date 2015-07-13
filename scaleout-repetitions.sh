56d83fee5d763fca0e76beebbc99d4e6bfff2d94
Willi <willi@jups42.de>
2015-07-13 20:08:26 +0200

    fix scaleout script

--- scaleout-repetitions.sh
+++ scaleout-repetitions.sh
@@ -1,10 +1,8 @@
 cd flink
 sudo -u hadoop10 /home/hadoop10/bin/start-flink-cluster.sh flink-0.9.0 && \
-batchStats.sh &> 2015-07-13-scaleout-repetitions-flink.log
+./batchStats.sh &> 2015-07-13-scaleout-repetitions-flink.log && \
 sudo -u hadoop10 /home/hadoop10/bin/stop-flink-cluster.sh flink-0.9.0 && \
-cd ..
-#####
-cd spark
+cd ../spark &&\
 sudo -u hadoop10 /home/hadoop10/bin/start-spark-cluster.sh && \
-batchStats-spark.sh &> 2015-07-13-scaleout-repetitions-spark.log
+./batchStats-spark.sh &> 2015-07-13-scaleout-repetitions-spark.log && \
 sudo -u hadoop10 /home/hadoop10/bin/stop-spark-cluster.sh
